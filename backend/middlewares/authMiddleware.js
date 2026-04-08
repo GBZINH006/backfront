fc2803d35e5328d80e5a9eb4691f778d82b1eefc
GBZINH006 <gabrieldossantosdutra06@gmail.com>
2026-04-08 09:33:57 -0300

    SASA

--- backend/middlewares/authMiddleware.js
+++ backend/middlewares/authMiddleware.js
@@ -1,17 +1,15 @@
 const jwt = require('jsonwebtoken');
 
-const authMiddleware = (req, res, next) => {
-    const token = req.header('Authorization')?.replace('Bearer ', '');
-    if (!token) {
-        return res.status(401).json({ message: 'Acesso negado. Token não providenciado.' });
-    }
-    try {
-        const decoded = jwt.verify(token, process.env.JWT_SECRET);
-        req.user = decoded;
-        next();
-    } catch (error) {
-        res.status(400).json({ message: 'Token inválido.' });
-    }
-};
+module.exports = (req, res, next) => {
+  const authHeader = req.headers.authorization;
+  if (!authHeader) return res.status(401).json({ error: 'Token não fornecido.' });
 
-module.exports = authMiddleware;
+  const token = authHeader.split(' ')[1];
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = decoded;
+    next();
+  } catch {
+    return res.status(401).json({ error: 'Token inválido.' });
+  }
+};
